d821b659100d8d78f0b9510ca5d3ec2992721d5d
BenjaminLeM <Benjamin.LeMaistre@georgebrown.ca>
2023-10-07 14:59:12 -0400

    Initial commit

---
+++ Arrays.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template<class T>
+class Array 
+{
+
+public:
+	//Constructor
+
+
+
+private:
+	// Private Variables
+	T* m_array;			// Pointer to the beginning of the array
+
+	int m_maxSize;		// Maximum size of the array
+	int m_growSize;		// Amount the array can grow through expansion
+	int m_numElements;	// Number of items currently in my array
+};
